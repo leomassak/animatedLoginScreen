477cdb00b4384bbd5686b4ff84d66f8aa857b6c6
Leonardo Massak <leoo.lcm23@hotmail.com>
2020-09-30 16:03:06 -0300

    Animations and layout added

---
+++ script.js
@@ -0,0 +1,62 @@
+const loginButton = document.querySelector('.btn-login');
+const form = document.querySelector('form');
+const squares = document.querySelector('.squares');
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const fields = [...document.querySelectorAll('.input-block input')];
+
+    fields.forEach(field => {
+        if(field.value === '') {
+            form.classList.add('validate-error');
+        }
+    });
+
+    const formError = document.querySelector('.validate-error');
+
+    if(formError) {
+        formError.addEventListener('animationend', (event) => {
+            if (event.animationName === 'taunt') {
+                formError.classList.remove('validate-error');
+            }
+        })
+    } else {
+        form.classList.add("form-hide");
+    }
+});
+
+form.addEventListener('animationstart', (event) => {
+    if(event.animationName === 'down') {
+        document.querySelector('body').style.overflow = 'hidden';
+    }
+})
+
+form.addEventListener('animationend', (event) => {
+    if (event.animationName === 'down') {
+        form.style.display = 'none';
+    }
+    document.querySelector('body').style.overflow = 'none';
+})
+
+for(let i = 0; i < 11; i += 1) {
+    const li = document.createElement('li');
+
+    const random = (min, max) => Math.random() * (max - min) + min;
+
+    const size = Math.floor(random(10, 120));
+    const position = random(1,99);
+    const delay = random(5, 0.1);
+    const duration = random(24, 12);
+
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+    li.style.left = `${position}%`;
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${duration}s`;
+    li.style.animationTimingFunction= `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+    squares.appendChild(li);
+}
+
